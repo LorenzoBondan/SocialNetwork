6f0c7d38460e376e55fb0de483a626820855abba
LorenzoBondan <lbbondan@gmail.com>
2023-05-27 10:54:58 -0300

    pagination and filter on users page

--- frontend/src/pages/Users/index.tsx
+++ frontend/src/pages/Users/index.tsx
@@ -7,27 +7,49 @@ import { requestBackend } from 'util/requests';
 import UserCard from 'Components/UserCard';
 import { AuthContext } from 'AuthContext';
 import { getTokenData, isAuthenticated } from 'util/auth';
+import UserFilter, { UserFilterData } from 'Components/UserFilter';
+import Pagination from 'Components/Pagination';
+
+type ControlComponentsData = {
+  activePage: number;
+  filterData: UserFilterData;
+}
 
 const Users = () => {
+
+    // pagination and filter
+    const [controlComponentsData, setControlComponentsData] = useState<ControlComponentsData>({activePage:0, filterData: { name: '' },});
+
+    const handlePageChange = (pageNumber : number) => {
+      setControlComponentsData({activePage: pageNumber, filterData: controlComponentsData.filterData});
+    }
+
+
 
     const [page, setPage] = useState<SpringPage<User>>();
 
     const getUsers = useCallback(() => {
-        const params : AxiosRequestConfig = {
-          method:"GET",
-          url: "/users",
-        }
-    
-        requestBackend(params) 
-          .then(response => {
-            setPage(response.data);
-            window.scrollTo(0, 0);
-          })
-      }, [])
+      const params : AxiosRequestConfig = {
+        method:"GET",
+        url: "/users",
+        params: {
+          page: controlComponentsData.activePage,
+          size: 4,
   
-      useEffect(() => {
-        getUsers();
-      }, [getUsers]);
+          name: controlComponentsData.filterData.name
+        },
+      }
+    
+      requestBackend(params) 
+        .then(response => {
+          setPage(response.data);
+          window.scrollTo(0, 0);
+        })
+    }, [controlComponentsData])
+  
+    useEffect(() => {
+      getUsers();
+    }, [getUsers]);
 
     // getting the email
     const { authContextData, setAuthContextData } = useContext(AuthContext);
@@ -72,8 +94,16 @@ const Users = () => {
         getUser();
     }, [getUser]);
 
+    const handleSubmitFilter = (data : UserFilterData) => {
+      setControlComponentsData({activePage: 0, filterData: data});
+    }
+
     return(
         <div className='users-container'>
+            <div className='users-search-bar-container'>
+              <UserFilter onSubmitFilter={handleSubmitFilter} />
+            </div>
+
             <div className="row" style={{width:"100%"}}>
                 {page?.content
                 .sort((a,b) => a.name > b.name ? 1 : -1)
@@ -83,10 +113,15 @@ const Users = () => {
                             <UserCard user={u} followerId={user?.id}/>
                         }
                     </div>
-                    )
-                )
-                }
+                ))}
             </div>
+
+            <Pagination 
+              pageCount={(page) ? page.totalPages : 0} 
+              range={2}
+              onChange={handlePageChange}
+              forcePage={page?.number}
+            />
 
         </div>
     );
